707b19db74205fb0f19320ef4f6f51041634c098
fathurazka <143975815+fathurazka@users.noreply.github.com>
2024-12-09 00:49:18 +0700

    HUD Added, Player Gesture text added

---
+++ Assets/Scripts/HUDController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+
+public class HUDController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI playerGestureText;
+    [SerializeField] private UDPReceive udpReceive;
+
+    void Update()
+    {
+        // Update the gesture text from UDP receiver
+        if (udpReceive != null && playerGestureText != null)
+        {
+            playerGestureText.text = $"{udpReceive.currentGesture.Substring(1, udpReceive.currentGesture.Length - 2)}";
+        }
+    }
+}
